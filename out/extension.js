123bba1a6236b897c13c295b5cf732ea837b403d
mistople <ddossett@microsoft.com>
2021-04-26 17:01:43 -0700

    Add status bar command

--- out/extension.js
+++ out/extension.js
@@ -3,19 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 function activate(context) {
-    let hello = vscode.commands.registerCommand('hello-world.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello VS Code!');
+    let hello = vscode.commands.registerCommand("hello-world.helloWorld", () => {
+        vscode.window.showInformationMessage("Hello VS Code!");
     });
-    let goodbye = vscode.commands.registerCommand('hello-world.goodbyeWorld', () => {
-        vscode.window.showInformationMessage('Goodbye!');
+    let goodbye = vscode.commands.registerCommand("hello-world.goodbyeWorld", () => {
+        vscode.window.showInformationMessage("Goodbye!");
     });
-    let commentCurrentLine = vscode.commands.registerCommand('hello-world.commentCurrentLine', () => {
-        vscode.commands.executeCommand('editor.action.addCommentLine');
+    let commentCurrentLine = vscode.commands.registerCommand("hello-world.commentCurrentLine", () => {
+        vscode.commands.executeCommand("editor.action.addCommentLine");
     });
-    let openDialog = vscode.commands.registerCommand('hello-world.openDialog', () => {
+    let openDialog = vscode.commands.registerCommand("hello-world.openDialog", () => {
         vscode.window.showOpenDialog();
     });
-    context.subscriptions.push(hello, goodbye, commentCurrentLine, openDialog);
+    let setStatus = vscode.commands.registerCommand("hello-world.setStatus", () => {
+        vscode.window.setStatusBarMessage("This is really long text that will probably eventually overflow if it goes on long enough!!!");
+    });
+    context.subscriptions.push(hello, goodbye, commentCurrentLine, openDialog, setStatus);
 }
 exports.activate = activate;
 function deactivate() { }
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,29 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	let hello = vscode.commands.registerCommand('hello-world.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello VS Code!');
-	});
+  let hello = vscode.commands.registerCommand("hello-world.helloWorld", () => {
+    vscode.window.showInformationMessage("Hello VS Code!");
+  });
 
-	let goodbye = vscode.commands.registerCommand('hello-world.goodbyeWorld', () => {
-		vscode.window.showInformationMessage('Goodbye!');
-	});
+  let goodbye = vscode.commands.registerCommand("hello-world.goodbyeWorld", () => {
+    vscode.window.showInformationMessage("Goodbye!");
+  });
 
-	let commentCurrentLine = vscode.commands.registerCommand('hello-world.commentCurrentLine', () => {
-		vscode.commands.executeCommand('editor.action.addCommentLine');
-	})
+  let commentCurrentLine = vscode.commands.registerCommand("hello-world.commentCurrentLine", () => {
+    vscode.commands.executeCommand("editor.action.addCommentLine");
+  });
 
-	let openDialog = vscode.commands.registerCommand('hello-world.openDialog', () => {
-		vscode.window.showOpenDialog();
-	})
+  let openDialog = vscode.commands.registerCommand("hello-world.openDialog", () => {
+    vscode.window.showOpenDialog();
+  });
 
-	context.subscriptions.push(hello, goodbye, commentCurrentLine, openDialog);
+  let setStatus = vscode.commands.registerCommand("hello-world.setStatus", () => {
+    vscode.window.setStatusBarMessage(
+      "This is really long text that will probably eventually overflow if it goes on long enough!!!"
+    );
+  });
+
+  context.subscriptions.push(hello, goodbye, commentCurrentLine, openDialog, setStatus);
 }
 
 export function deactivate() {}
